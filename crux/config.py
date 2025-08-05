014069b6b6ec686c7f4880a0a0205caec7f7bfdc
Abayesh.S <85433225+Abayesh@users.noreply.github.com>
2025-08-05 11:36:46 +0530

    Update config.py

--- crux/config.py
+++ crux/config.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv('credentials.env')
 
-GITHUB_PAT = os.getenv("GITHUB_PAT")
-GITHUB_REPO = os.getenv("GITHUB_REPO")
 FAB_API_URL = os.getenv("FAB_API_URL")
 FAB_USER_ID = os.getenv("FAB_USER_ID")
 FAB_API_KEY = os.getenv("FAB_API_KEY")
